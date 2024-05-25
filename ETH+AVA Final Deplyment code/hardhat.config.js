effce3b5c14dedafc6cfc5f4ced287bf38634b89
Devansh Sharma <souldestryr@gmail.com>
2024-05-25 18:09:26 +0530

    Add files via upload

---
+++ ETH+AVA Final Deplyment code/hardhat.config.js
@@ -0,0 +1,52 @@
+
+require("@nomicfoundation/hardhat-toolbox");
+
+const FORK_FUJI = true;
+const FORK_MAINNET = false;
+let forkingData = undefined;
+
+if (FORK_MAINNET) {
+  forkingData = {
+    url: "https://api.avax.network/ext/bc/C/rpcc",
+  };
+}
+if (FORK_FUJI) {
+  forkingData = {
+    url: "https://api.avax-test.network/ext/bc/C/rpc",
+  };
+}
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.20",
+  networks: {
+    hardhat: {
+      gasPrice: 225000000000,
+      chainId: !forkingData ? 43112 : undefined, //Only specify a chainId if we are not forking
+      forking: forkingData
+    },
+    fuji: {
+      url: 'https://api.avax-test.network/ext/bc/C/rpc',
+      gasPrice: 225000000000,
+      chainId: 43113,
+      accounts: [
+        // YOUR PRIVATE KEY HERE
+      ]
+    },
+    mainnet: {
+      url: 'https://api.avax.network/ext/bc/C/rpc',
+      gasPrice: 225000000000,
+      chainId: 43114,
+      accounts: [
+        // YOUR PRIVATE KEY HERE
+      ]
+    }
+  }
+}
+
+module.exports = {
+  // ...rest of the config...
+  etherscan: {
+    apiKey: "4454a0f7aca8a50bdbe2e02e93b2e78869030ddddc18ba469e69257ceaf616ea",
+  },
+};
