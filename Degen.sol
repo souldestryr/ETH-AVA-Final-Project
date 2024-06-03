5eb8f33fa5856abd7689fc8e7a55a9c3bda57dd0
Devansh Sharma <souldestryr@gmail.com>
2024-06-03 14:08:35 +0530

    Update Degen.sol

--- Degen.sol
+++ Degen.sol
@@ -5,6 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Degen is ERC20{
     event Redeem(address indexed from ,uint indexed amount);
+    mapping(string=>uint) public i_price;
     constructor() ERC20("Degen", "DGN") {}
 
         function mint(address _owner, uint amount) public {
@@ -15,12 +16,16 @@ contract Degen is ERC20{
             _transfer(_msgSender(),reciver,amount);
             return true;
     }
-    function redeem(address from,uint amount)public returns(uint){
-            require (balanceOf(from)>amount,"Insufficient Balance");
-            uint level = amount;
-            _burn(from, amount);
-            emit Redeem(from,amount);
-            return level;
+    function redeem(string memory i_name, address from) public returns (uint) {
+        uint price = i_price[i_name];
+        require(balanceOf(from) >= price, "Insufficient Balance");
+        _burn(from, price);
+        emit Redeem(from,price);
+        return price;
+    }
+    function setItemPrice(string memory i_name, uint price,address _owner) public {
+        require(_owner==msg.sender,"Only Owner can set price of items");
+        i_price[i_name] = price;
     }
     function token_balance(address from)public view returns(uint){
         return balanceOf(from);
